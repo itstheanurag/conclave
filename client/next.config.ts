4afa3d746ab5f207150a38b006ca3dc6ede89313
gaurav <gauravanurag36@gmail.com>
2025-10-23 13:17:23 +0530

    feat: do installations and update configs

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  swcMinify: false,
+  compress: false,
+  optimizeFonts: true,
 };
 
 export default nextConfig;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,22 @@ import daisyui from "daisyui";
 const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      colors: {
+        primary: "#1c4ed8",
+        secondary: "#9333ea",
+        accent: "#f59e0b",
+        neutral: "#1f2937",
+        "base-100": "#ffffff",
+        info: "#3b82f6",
+        success: "#10b981",
+        warning: "#f97316",
+        error: "#ef4444",
+      },
+    },
   },
   plugins: [daisyui, require("@tailwindcss/typography")],
   daisyui: {},
